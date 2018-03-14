f1d597cfb05b8ad419833b659b3887211dff4a3d
wzheng <hb_wangzheng@163.com>
2018-03-14 15:51:12 +0800

    add assets

---
+++ src/pages/city/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{tittle}}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tittle: 'CITY'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
