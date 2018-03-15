0938d37669deb42ec7403623bfd9be3ca2c22404
wzheng <hb_wangzheng@163.com>
2018-03-15 11:02:11 +0800

    add list page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -21,6 +21,30 @@
         </li>
       </ul>
     </div>
+
+    <div class="h-recommend">
+      <h2>推荐</h2>
+      <ul class="h-list">
+        <li v-for="(item,index) in recommendList" :key="index" v-bind:class="{noborder: index==recommendList.length-1}">
+          <div class="h-cover">
+            <img :src="item.img" :alt="item.tittle">
+          </div>
+          <div class="h-info">
+            <div class="h-tittle">{{item.tittle}}</div>
+            <div class="h-cut">{{item.cut}}</div>
+            <div class="h-minor">
+              <span class="price-total">{{item.totalPrice}}</span>
+              <span class="unit-price">{{item.unitPrice}}</span>
+            </div>
+            <div class="tag-box">
+              <span class="tag" v-bind:class="{tagblue:tag.type==1,tagyellow:tag.type==2,taggreen:tag.type==3,tagbabyblue:tag.type==4}" v-for="(tag,i) in item.tags" :key="i">{{tag.tag}}</span>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <button class="btn-more" @click="goTo('../list/list')">查看更多</button>
+    </div>
+
     <div class="userinfo" @click="bindViewTap">
       <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
       <div class="userinfo-nickname">
@@ -44,7 +68,7 @@
 
 <script>
 import card from '@/components/card'
-
+import '../../assets/css/base.css'
 export default {
   data() {
     return {
@@ -75,6 +99,48 @@ export default {
           iocnUrl:
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
           url: ''
+        }
+      ],
+      recommendList: [
+        {
+          img:
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          tittle: '国展经典两房 满二税少 居家安静，双地铁口物业',
+          cut: '2室1厅/50m²/东北/布吉关',
+          totalPrice: '300万',
+          unitPrice: '60000元/平',
+          tags: [
+            { type: 1, tag: '地铁' },
+            { type: 2, tag: '满两年' },
+            { type: 4, tag: '随时看房' },
+            { type: 3, tag: '有电梯' }
+          ]
+        },
+        {
+          img:
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          tittle: '国展经典两房 满二税少 居家安静，双地铁口物业',
+          cut: '2室1厅/150m²/东北/布吉关',
+          totalPrice: '2300万',
+          unitPrice: '5400元/平',
+          tags: [
+            { type: 1, tag: '地铁' },
+            { type: 2, tag: '满两年' },
+            { type: 4, tag: '随时看房' }
+          ]
+        },
+        {
+          img:
+            'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          tittle: '国展经典两房 满二税少 居家安静，双地铁口物业',
+          cut: '2室1厅/100m²/东北/布吉关',
+          totalPrice: '540万',
+          unitPrice: '23000元/平',
+          tags: [
+            { type: 1, tag: '地铁' },
+            { type: 4, tag: '随时看房' },
+            { type: 2, tag: '满两年' }
+          ]
         }
       ]
     }
@@ -143,8 +209,14 @@ export default {
 </script>
 
 <style scoped>
+/* 顶部搜索 */
 .top-search {
   position: relative;
+  width: 100%;
+}
+.top-search img {
+  width: 100%;
+  height: 200px;
 }
 .search {
   background: #fff;
@@ -185,13 +257,64 @@ export default {
 .menu li .icon-menu {
   text-align: center;
 }
-.menu li p {
-}
 .menu li .icon-menu img {
   width: 40px;
   height: 40px;
   border-radius: 50%;
 }
+/* 推荐 */
+.h-recommend {
+  width: 100%;
+}
+.h-recommend .h-list li {
+  display: flex;
+  padding: 20px 0;
+  border-bottom: 1px solid #ccc;
+}
+.h-recommend .h-list li.noborder {
+  border-bottom: none;
+}
+.h-list li .h-cover {
+  width: 40%;
+}
+.h-list .h-cover img {
+  width: 100%;
+  height: 75%;
+}
+.h-list .h-info {
+  padding-left: 20px;
+}
+.h-list .h-info .h-tittle {
+  font-size: 18px;
+  line-height: 24px;
+  color: #000;
+}
+
+.h-list .h-info .h-cut {
+  font-size: 14px;
+  margin: 4px 0;
+  color: #8c8c8c;
+}
+.h-list .h-info .h-minor {
+  font-size: 15px;
+  margin: 4px 0;
+}
+.h-list .h-info .h-minor .price-total {
+  font-size: 18px;
+  color: red;
+  margin-right: 5px;
+}
+.h-list .h-info .h-minor .unit-price {
+  color: #666;
+}
+.h-list .h-info .tag-box {
+  font-size: 14px;
+}
+.h-recommend .btn-more {
+  border: none;
+  background: #ccc;
+}
+
 .userinfo {
   display: flex;
   flex-direction: column;
---
+++ src/pages/list/index.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="container">
+    <!-- 列表 -->
+    <div class="search">
+      <i class="icon"></i>
+      <input type="text" placeholder="请输入小区名或者商圈名">
+    </div>
+    <ul class="queryList">
+      <li>售价
+        <i class=""></i>
+      </li>
+      <li>售价</li>
+      <li>售价</li>
+      <li>售价</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.search {
+  width: 100%;
+  background: #eee;
+  padding: 10px 0px;
+  border-radius: 5px;
+}
+.search .icon {
+  font-size: 12px;
+}
+.search input {
+  margin-left: 20px;
+  font-size: 16px;
+}
+.queryList {
+  display: flex;
+  width: 100%;
+}
+.queryList li {
+  width: 25%;
+  text-align: center;
+}
+</style>
+
